870e55810a1dfda27ae0e2cbc38b8dcb0e5372c4
quyet <=>
2022-11-16 01:10:26 +0700

    init chart

--- src/app/merchant/dashboard/dashboard.component.ts
+++ src/app/merchant/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -9,7 +9,35 @@ export class DashboardComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit()
+  {
+    var myChart = new Chart("myChart", {
+      type: 'line',
+      data: {
+        labels: ['sunday', 'monday', 'tuesday', 'webnesday', 'Thursday', 'friday', 'saturday'],
+        datasets: [{
+          label: 'Data1',
+          data: [12, 19, 3, 5, 2, 3,0],
+          backgroundColor:"#151617",
+          borderColor: "#0196FD",
+          borderWidth: 1
+        },
+          {
+            label: 'Dat21',
+            data: [19, 12, 5, 10, 1, 6,15],
+            backgroundColor:"#d51c1c",
+            borderColor: "#FFAF00",
+            borderWidth: 1
+          }]
+      },
+      options: {
+        scales: {
+          // y: {
+          //   beginAtZero: true
+          // }
+        }
+      }
+    });
   }
 
 }

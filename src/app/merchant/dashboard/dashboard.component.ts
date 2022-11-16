0747888e8e3f82eb39495fb7ce8f3e2e90b68b34
quyet <=>
2022-11-16 16:35:14 +0700

    done

--- src/app/merchant/dashboard/dashboard.component.ts
+++ src/app/merchant/dashboard/dashboard.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Chart } from 'chart.js';
+import {Component, OnInit} from '@angular/core';
+import {Chart} from 'chart.js';
 import {ProductService} from "../../service/product/product.service";
 import {Product} from "../../model/product";
+import {ChartService} from "../../service/chart/chart.service";
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -9,57 +11,56 @@ import {Product} from "../../model/product";
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private productService: ProductService) {
-
-  }
-nameProducts: string[]=["hao","hihi","hi","sal","hd","hihi","hi","sal","hd","hao","hihi","hi","sal","hd","hihi","hi","sal","hd"];
-  numberOrder: number[]=[0,9,8,7,6,7,10,3,7,0,9,8,7,6,7,10,3,7];
-  nameProduct: string[]=[];
-  nuberOrder: number[]=[];
-  ngOnInit()
-  {
+  constructor(private productService: ProductService,
+              private chartService: ChartService) {
+    this.color = "red";
+    this.type= "bar"
     console.log(this.nameProducts)
     console.log(this.numberOrder)
-    this.productService.getAllProductByMerchant(JSON.parse(localStorage.getItem("data")!).merchant.id).subscribe((product)=>{
-    console.log(product)
-    product.forEach(item=>{
-      this.nameProduct.push(item.name);
-      this.nuberOrder.push(Number(item.numberOrder))
-      console.log(this.nameProduct)
-      console.log(this.nuberOrder)
+    this.productService.getAllProductByMerchant(JSON.parse(localStorage.getItem("data")!).merchant.id).subscribe((product) => {
+      console.log(product)
+      product.forEach(item => {
+        this.nameProducts.push(item.name);
+        this.numberOrder.push(Number(item.numberOrder))
+      })
+      var myChart = this.chartService.chart(this.nameProducts, this.numberOrder, "Số bán ra",this.type, this.color, "black")
+
     })
-      var myChart = new Chart("myChart", {
-        type: 'bar',
-        data: {
-          labels: this.nameProduct,
-          datasets: [{
-            label: 'Data1',
-            data: this.nuberOrder,
-            backgroundColor:"#e10c0c",
-            borderColor: "#fd0172",
-            borderWidth: 3
-          },
-            // {
-            //   label: 'Dat21',
-            //   data: [19, 12, 5, 10, 1, 6,15],
-            //   backgroundColor:"#d51c1c",
-            //   borderColor: "#FFAF00",
-            //   borderWidth: 1
-            // }
-          ]
-        },
-        options: {
-          scales: {
-            // y: {
-            //   beginAtZero: true
-            // }
-          }
-        }
-      });
+  }
 
-  })
+  nameProducts: string[] = [];
+  numberOrder: number[] = [];
+  color: any;
+  type:any;
 
+  ngOnInit() {
 
   }
 
+setType(){
+    this.type="bar"
+  this.setColor(this.color)
+}
+  setColor(element: any) {
+ this.nameProducts=[]
+    this.numberOrder=[]
+    this.color = element
+    console.log(this.color)
+    console.log(this.nameProducts)
+    console.log(this.numberOrder)
+    this.productService.getAllProductByMerchant(JSON.parse(localStorage.getItem("data")!).merchant.id).subscribe((product) => {
+      console.log(product)
+      product.forEach(item => {
+        this.nameProducts.push(item.name);
+        this.numberOrder.push(Number(item.numberOrder))
+      })
+      var myChart = this.chartService.chart(this.nameProducts, this.numberOrder, "Số bán ra",this.type, this.color, "black")
+
+    })
+  }
+
+  convert() {
+    this.type="radar"
+    this.setColor(this.color)
+  }
 }
